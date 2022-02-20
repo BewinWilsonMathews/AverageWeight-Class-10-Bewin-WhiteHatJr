f83b78d82174e00e04e5b5126a6523aceba55015
BewinWilsonMathews <95472327+BewinWilsonMathews@users.noreply.github.com>
2022-02-20 22:08:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var weight = [37,41,35,10];
+var su;
+var avg;
+function setup() 
+{
+  createCanvas(400,400);
+  sum =weight[0],weight[1],weight[2],weight[3]
+  avg =sum/weight.length
+  console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
